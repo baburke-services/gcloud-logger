d1e5554ecec4623b304e1f3b2bca0526e582c21f
Bryan Burke <lunarprog@fb.com>
2019-06-10 01:20:04 -0700

    adding json-string parsing

--- glogger/glogger.go
+++ glogger/glogger.go
@@ -1,23 +1,48 @@
 package glogger
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type LogFeeder func(<-chan interface{})
+
+type LogReader struct {
+	events chan interface{}
+}
+
 const (
 	EVENT_CHANNEL_SIZE = 25
 )
 
-type JsonReader struct {
-	events <-chan interface{}
-}
-
-func NewJsonReader(channel_size int) *JsonReader {
+func NewLogReader(channel_size int) *LogReader {
 	if channel_size < 0 {
 		channel_size = EVENT_CHANNEL_SIZE
 	}
 
 	events := make(chan interface{}, channel_size)
 
-	reader := JsonReader{events: events}
+	reader := LogReader{events: events}
 
 	return &reader
+}
+
+func (r *LogReader) FromJsonBytes(s []byte) <-chan interface{} {
+	var data interface{}
+
+	// defining inline as a special case that still works like the others
+	function := func(c chan<- interface{}) {
+		if err := json.Unmarshal(s, &data); err != nil {
+			fmt.Errorf("unmarshal error: %q\n", err)
+		}
+
+		c <- data
+		close(c)
+	}
+
+	go function(r.events)
+
+	return r.events
 }
 
 // vim: noexpandtab
--- glogger/glogger_test.go
+++ glogger/glogger_test.go
@@ -4,21 +4,42 @@ import (
 	"testing"
 )
 
-func TestNewJsonReaderNoSize(t *testing.T) {
-	var reader *JsonReader
-	reader = NewJsonReader(-1)
+func TestNewLogReaderNoSize(t *testing.T) {
+	var reader *LogReader
+	reader = NewLogReader(-1)
 
 	if cap(reader.events) != EVENT_CHANNEL_SIZE {
 		t.Errorf("event channel size: %d", cap(reader.events))
 	}
 }
 
-func TestNewJsonReaderSize(t *testing.T) {
-	var reader *JsonReader
-	reader = NewJsonReader(10)
+func TestNewLogReaderSize(t *testing.T) {
+	var reader *LogReader
+	reader = NewLogReader(10)
 
 	if cap(reader.events) != 10 {
 		t.Errorf("event channel size: %d", cap(reader.events))
+	}
+}
+
+func TestFromJsonString(t *testing.T) {
+	reader := NewLogReader(-1)
+	bytes := []byte(`"whatever test"`)
+	ch := reader.FromJsonBytes(bytes)
+	var data interface{}
+	var ok bool
+	var s string
+
+	data = <-ch
+	s, ok = data.(string)
+	if !ok {
+		t.Errorf("returned data is not a string!\n")
+	} else if s != "whatever test" {
+		t.Errorf("got back the wrong string: %s\n", s)
+	}
+	data, ok = <-ch
+	if ok {
+		t.Errorf("this was not the only item on the channel!")
 	}
 }
 
