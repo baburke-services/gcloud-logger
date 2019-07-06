81d872f054debbe33bc9e4a8e9aefbc149a8da3c
Bryan Burke <lunarprog@fb.com>
2019-07-06 15:17:28 -0700

    adding tests

--- glogger/glogger_test.go
+++ glogger/glogger_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestReaderJsonMap(t *testing.T) {
+func TestReadEntryDefaults(t *testing.T) {
 	json_bytes := []byte("{\"output\": \"hello\"}")
 	bytes_reader := bytes.NewReader(json_bytes)
 	log_reader := NewLogReader(bytes_reader)
@@ -35,6 +35,83 @@ func TestReaderJsonMap(t *testing.T) {
 	if entry.LevelName != LOG_LEVELS[DEFAULT_LOG_LEVEL] {
 		t.Errorf("LevelName == %q; expected %q", entry.LevelName, LOG_LEVELS[DEFAULT_LOG_LEVEL])
 	}
+}
+
+func TestReadEntryCursor(t *testing.T) {
+	json_bytes := []byte(`{
+		"__CURSOR": "test cursor point"
+	}`)
+	bytes_reader := bytes.NewReader(json_bytes)
+	log_reader := NewLogReader(bytes_reader)
+	entry, err := log_reader.read_entry()
+	if err != nil {
+		t.Fatal("failed to read a single entry")
+	}
+
+	if entry.Cursor != "test cursor point" {
+		t.Errorf("Cursor == %q; expected %q", entry.Cursor, "test cursor point")
+	}
+}
+
+func TestReadEntryMessage(t *testing.T) {
+	json_bytes := []byte(`{
+		"MESSAGE": "test message"
+	}`)
+	bytes_reader := bytes.NewReader(json_bytes)
+	log_reader := NewLogReader(bytes_reader)
+	entry, err := log_reader.read_entry()
+	if err != nil {
+		t.Fatal("failed to read a single entry")
+	}
+
+	if entry.Message != "test message" {
+		t.Errorf("Message == %q; expected %q", entry.Message, "test message")
+	}
+}
+
+func TestReadEntryLogLevel(t *testing.T) {
+	json_bytes := []byte(`{
+		"PRIORITY": "2"
+	}`)
+	bytes_reader := bytes.NewReader(json_bytes)
+	log_reader := NewLogReader(bytes_reader)
+	entry, err := log_reader.read_entry()
+	if err != nil {
+		t.Fatal("failed to read a single entry")
+	}
+
+	if entry.Level != 2 {
+		t.Errorf("Log Level == %q; expected %q", entry.Level, 2)
+	}
+
+	if entry.LevelName != LOG_LEVELS[2] {
+		t.Errorf("Log Level Name == %q; expected %q", entry.LevelName, LOG_LEVELS[2])
+	}
+}
+
+func TestStartStream(t *testing.T) {
+	json_bytes := []byte(`{
+		"PRIORITY": "2"
+	}`)
+	bytes_reader := bytes.NewReader(json_bytes)
+	log_reader := NewLogReader(bytes_reader)
+
+	ch := log_reader.StartStream(0)
+	entry, ok := <-ch
+
+	if !ok {
+		t.Fatalf("no events on channel")
+	}
+
+	if entry.Level != 2 {
+		t.Errorf("Log Level == %q; expected %q", entry.Level, 2)
+	}
+
+	entry, ok = <-ch
+	if ok {
+		t.Errorf("channel not closed")
+	}
+
 }
 
 // vim: noexpandtab
