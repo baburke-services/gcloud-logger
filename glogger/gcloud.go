081290038043f04b22a85843098b5f4a46c03f41
Bryan Burke <bburke@baburke.net>
2019-09-01 19:02:38 -0700

    determining project automatically

--- glogger/gcloud.go
+++ glogger/gcloud.go
@@ -1,15 +1,42 @@
 package glogger
 
 import (
-	"cloud.google.com/go/logging"
 	"context"
 	"log"
 	"os"
+
+	"cloud.google.com/go/logging"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
+
+const (
+	PROJECT_DEFAULT = "baburke-services"
+)
+
+func get_project_id() string {
+	project := os.Getenv("GLOGGER_PROJECT")
+	if project != "" {
+		return project
+	}
+
+	ctx := context.Background()
+	credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeScope)
+	if err != nil {
+		log.Printf("warning: %v", err)
+	}
+
+	if credentials.ProjectID != "" {
+		return credentials.ProjectID
+	}
+
+	log.Printf("warning: returning builtin default for project")
+	return PROJECT_DEFAULT
+}
 
 func NewGLogger() (*logging.Logger, *logging.Client, error) {
 	ctx := context.Background()
-	projectID := "baburke-services"
+	projectID := get_project_id()
 
 	client, err := logging.NewClient(ctx, projectID)
 	if err != nil {
